d27395b29fd53709196a9626f41522f1d5a2369b
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2024-01-31 14:56:05 -0500

    feat: :sparkles: added loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,10 @@
+import { spiral } from "ldrs";
 
 export const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+    spiral.register();
+    return (
+        <div className="container-loader">
+            <l-spiral size="40" speed="0.9" color="black"></l-spiral>
+        </div>
+    );
+};
